cb3db097ede8570d068e79cc71e59c79cab86c04
Michael Huo <90225549+huo1m1w1@users.noreply.github.com>
2023-06-28 12:53:54 +0100

    Update data_test.py

--- test/data_test.py
+++ test/data_test.py
@@ -48,7 +48,38 @@ class TestNFTScraper(unittest.TestCase):
             "Items",
         ]
         self.assertEqual(list(self.scraper.table.columns), expected_columns)
+    @patch("src.data_scraping.NFTScraper.collect_screen_data")
+    def test_extract_text_from_data(self, mock_collect_screen_data):
+        element = Mock()
+        element.text = "1\nCollection 1\n100\n\n0.1\n50\n50%\n10"
+        mock_collect_screen_data.return_value = [element]
+        data = self.scraper.extract_text_from_data([element])
+        expected_data = ["1", "Collection 1", "100", "", "0.1", "50", "50%", "10"]
+        self.assertEqual(data, expected_data)
 
+    @patch("src.data_scraping.NFTScraper.slice_table_data")
+    def test_convert_row_data_to_table(self, mock_slice_table_data):
+        mock_slice_table_data.return_value = [
+            ["1", "Collection 1", "100", "+10%", "0.1", "50", "50%", "10", "25%", "20"],
+            ["2", "Collection 2", "200", "-5%", "0.2", "30", "40%", "8", "20%", "15"],
+        ]
+        data = self.scraper.convert_row_data_to_table(
+            mock_slice_table_data.return_value
+        )
+        expected_rank = 1
+        expected_collection = "Collection 1"
+        expected_shape = (2, 10)
+
+        self.assertEqual(int(data.iloc[0]["Rank"]), expected_rank)
+        self.assertEqual(data.iloc[0]["Collection"], expected_collection)
+        self.assertEqual(data.shape, expected_shape)
+
+    @patch("src.data_scraping.NFTScraper.collect_screen_data")
+    def test_filling_missing_data(self, mock_collect_screen_data):
+        data = ["1", "Collection 1", "100", "", "0.1", "50", "50%", "10"]
+        expected_data = ["1", "Collection 1", "100", "", "0.1", "50", "50%", "10"]
+        self.scraper.filling_missing_data(data)
+        self.assertEqual(data, expected_data)
 
 
 if __name__ == "__main__":
